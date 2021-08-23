3e0ad556b786fb157b5efb3507f234fa2b478cf9
Vicente Calderón <thunderwearsvicho37@protonmail.com>
2021-08-23 16:38:18 -0400

    Game

---
+++ Models/Game.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightPuzzleSolver.Models {
+	sealed class Game {
+		public Grid CurrentState {
+			get; set;
+		}
+	}
+}
