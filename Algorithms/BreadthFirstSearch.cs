ad3d18830d8d762839d9f3524f23833a732fa72b
Vicente Calderón <thunderwearsvicho37@protonmail.com>
2021-09-02 16:52:22 -0400

    Pathfinding interface

---
+++ Algorithms/BreadthFirstSearch.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightPuzzleSolver.Algorithms;
+/// <summary>
+/// BFS pathfinding algorithm
+/// </summary>
+internal class BreadthFirstSearch : IPathFinding {
+	/// <summary>
+	/// Starts a BFS pathfinding algorithm
+	/// </summary>
+	public BreadthFirstSearch() {
+
+	}
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="combination"></param>
+	public void Solve(List<string> combination) => throw new NotImplementedException();
+}
--- Algorithms/IPathFinding.cs
+++ Algorithms/IPathFinding.cs
@@ -1,6 +1,13 @@
 ﻿using System.Collections.Generic;
 
 namespace EightPuzzleSolver.Algorithms;
+/// <summary>
+/// This is a path finding algorithm
+/// </summary>
 interface IPathFinding {
-	void Solve(List<string> numbers);
+	/// <summary>
+	/// Solve a board
+	/// </summary>
+	/// <param name="numbers">The board combination</param>
+	void Solve(List<string> combination);
 }
